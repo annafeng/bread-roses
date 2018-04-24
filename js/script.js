7a3839c7472e7862aae44503cc1dde87d405f4b0
anna <annafeng1@gmail.com>
2018-04-23 22:38:01 -0400

    UP NEXT is working

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 $(window).on('load', function() {
 
+
+	// TITLE CARD
 	setTimeout(function() {
 
 		// after 2s, show video
 		// in the meantime, show card
 		setTimeout(function() {
-			$('.title-card').fadeOut()
+			$('.title_card').fadeOut()
 		}, 0)
 
 
@@ -13,4 +15,30 @@ $(window).on('load', function() {
 
 	}, 2000);
 
+
+		// MORE VIDEOS
+		var vid = document.getElementById('LIASS');
+
+		vid.addEventListener("ended", hideVideo, false);
+
+		function hideVideo() {
+			var vid = document.getElementById('LIASS');
+			var more = document.getElementById('up_next');
+			vid.removeEventListener("ended", hideVideo, false);
+			vid.style.display = 'none';
+			more.style.display = 'block';
+		}
+
+
+		$('#LIASS_02').click(function() {
+			$('#LIASS').attr('src','videos/LIASS_02.mp4');
+			$('#LIASS').get(0).play();
+			$('#up_next').hide();
+			$('#LIASS').show();
+		});
+
 });
+
+
+
+
