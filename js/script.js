6fd5ee7748322360c8c76c9b75015937d0e99d44
anna <annafeng1@gmail.com>
2018-04-23 23:06:16 -0400

    *

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ $(window).on('load', function() {
 
 		function hideVideo() {
 			var vid = document.getElementById('LIASS');
-			var more = document.getElementById('up_next');
+			var more = document.getElementById('related_videos');
 			vid.removeEventListener("ended", hideVideo, false);
 			vid.style.display = 'hidden';
 			more.style.display = 'block';
@@ -32,44 +32,44 @@ $(window).on('load', function() {
 
 		$('#LIASS_02').click(function() {
 			$('#LIASS').attr('src','videos/LIASS_02.mp4').get(0).play();
-			$('#up_next').hide();
+			$('#related_videos').hide();
 			$('#LIASS').show();
 		});
 
 		$('#LIASS_03').click(function() {
 			$('#LIASS').attr('src','videos/LIASS_03.mp4').get(0).play();
-			$('#up_next').hide();
+			$('#related_videos').hide();
 			$('#LIASS').show();
 		});
 
 		$('#LIASS_04').click(function() {
 			$('#LIASS').attr('src','videos/LIASS_04.mp4').get(0).play();
-			$('#up_next').hide();
+			$('#related_videos').hide();
 			$('#LIASS').show();
 		});
 
 		$('#LIASS_05').click(function() {
 			$('#LIASS').attr('src','videos/LIASS_05.mp4').get(0).play();
-			$('#up_next').hide();
+			$('#related_videos').hide();
 			$('#LIASS').show();
 		});
 
 		$('#LIASS_06').click(function() {
 			$('#LIASS').attr('src','videos/LIASS_06.mp4').get(0).play();
-			$('#up_next').hide();
+			$('#related_videos').hide();
 			$('#LIASS').show();
 		});
 
 		$('#LIASS_07').click(function() {
 			$('#LIASS').attr('src','videos/LIASS_07.mp4').get(0).play();
-			$('#up_next').hide();
+			$('#related_videos').hide();
 			$('#LIASS').show();
 		});
-		
+
 
 		document.getElementById('LIASS').addEventListener('ended',function(){
 			console.log(' video ended ');
-			$('#up_next').show();
+			$('#related_videos').show();
 		}, false);
 
 });
