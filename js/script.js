91b8e19b6fe31db21cafcc2bb29e46e420491efe
anna <annafeng1@gmail.com>
2018-04-25 17:55:11 -0400

    I AM REVERTING

--- js/script.js
+++ js/script.js
@@ -94,6 +94,7 @@ $(window).on('load', function() {
 		var thumbLIASS = relatedThumbnails[Math.floor(Math.random() * relatedThumbnails.length)];
 
 		var templateLIASS = randomTemplate[Math.floor(Math.random() * randomTemplate.length)];
+<<<<<<< HEAD
 
 		console.log( templateLIASS )
 
@@ -120,6 +121,8 @@ $(window).on('load', function() {
 
 	    
 	   
+=======
+>>>>>>> parent of 7dc7ce1... Removed #related_videos div
 
 
 
@@ -142,6 +145,7 @@ $(window).on('load', function() {
 
 		// vid.addEventListener("ended", showRelated, false);
 
+<<<<<<< HEAD
 		// function showRelated() {
 		// 	var vid = document.getElementById('LIASS');
 		// 	var more = document.getElementsByClassName('col_3');
@@ -149,10 +153,20 @@ $(window).on('load', function() {
 		// 	// vid.style.display = 'hidden';
 		// 	more.style.display = 'block';
 		// }
+=======
+		function showRelated() {
+			var vid = document.getElementById('LIASS');
+			var more = document.getElementById('related_videos');
+			vid.removeEventListener("ended", showRelated, false);
+			// vid.style.display = 'hidden';
+			more.style.display = 'block';
+		}
+>>>>>>> parent of 7dc7ce1... Removed #related_videos div
 
 
 		// // clicking on a thumbnail > changes src > hides thumbnails > shows video
 
+<<<<<<< HEAD
 		// $('#LIASS_02').click(function() {
 		// 	$('#LIASS').attr('src','videos/LIASS_02.mp4').get(0).play();
 		// 	$('.col_3').hide();
@@ -188,14 +202,58 @@ $(window).on('load', function() {
 		// 	$('.col_3').hide();
 		// 	$('#LIASS').show();
 		// });
+=======
+		$('#LIASS_02').click(function() {
+			$('#LIASS').attr('src','videos/LIASS_02.mp4').get(0).play();
+			$('#related_videos').hide();
+			$('#LIASS').show();
+		});
+
+		$('#LIASS_03').click(function() {
+			$('#LIASS').attr('src','videos/LIASS_03.mp4').get(0).play();
+			$('#related_videos').hide();
+			$('#LIASS').show();
+		});
+
+		$('#LIASS_04').click(function() {
+			$('#LIASS').attr('src','videos/LIASS_04.mp4').get(0).play();
+			$('#related_videos').hide();
+			$('#LIASS').show();
+		});
+
+		$('#LIASS_05').click(function() {
+			$('#LIASS').attr('src','videos/LIASS_05.mp4').get(0).play();
+			$('#related_videos').hide();
+			$('#LIASS').show();
+		});
+
+		$('#LIASS_06').click(function() {
+			$('#LIASS').attr('src','videos/LIASS_06.mp4').get(0).play();
+			$('#related_videos').hide();
+			$('#LIASS').show();
+		});
+
+		$('#LIASS_07').click(function() {
+			$('#LIASS').attr('src','videos/LIASS_07.mp4').get(0).play();
+			$('#related_videos').hide();
+			$('#LIASS').show();
+		});
+>>>>>>> parent of 7dc7ce1... Removed #related_videos div
 
 
 		// // display related videos again
 
+<<<<<<< HEAD
 		// document.getElementById('LIASS').addEventListener('ended',function(){
 		// 	console.log(' video ended ');
 		// 	$('.col_3').show();
 		// }, false);
+=======
+		document.getElementById('LIASS').addEventListener('ended',function(){
+			console.log(' video ended ');
+			$('#related_videos').show();
+		}, false);
+>>>>>>> parent of 7dc7ce1... Removed #related_videos div
 
 
 
